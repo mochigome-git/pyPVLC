6daadbacbb3c36868dccfa8a23b9f936035def7c
IT Department <106659178+mochigome-git@users.noreply.github.com>
2024-11-22 09:12:27 +0800

    ntgt12

--- source/build.bat
+++ source/build.bat
@@ -1,3 +1,6 @@
-python -m PyInstaller --onefile --windowed --add-data "accept.png;." --add-data "close.png;." --add-data "config.ini;." --add-data ".env;." main.py
-copy close.png dist
-copy accept.png dist
+:: python -m PyInstaller --onefile --windowed --add-data "accept.png;." --add-data "close.png;." --add-data "config.ini;." --add-data ".env;." main.py
+
+python -m PyInstaller --onedir --windowed main.py
+
+:: copy close.png dist
+:: copy accept.png dist
